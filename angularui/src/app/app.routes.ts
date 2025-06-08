c6a76d3e96a4ecde921bfb83e6724e07779b5a81
MduduziS <mduduzis@automationexchange.co.za>
2025-06-08 22:37:40 +0200

    login function

--- angularui/src/app/app.routes.ts
+++ angularui/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '' ,
@@ -9,4 +10,5 @@ export const routes: Routes = [
   },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
