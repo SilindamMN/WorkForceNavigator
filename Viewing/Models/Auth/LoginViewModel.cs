0d6b1fad082052fdfb385a704dc24a0e15dd1f4a
MduduziS <mduduzis@automationexchange.co.za>
2025-10-07 18:52:57 +0200

    add mvc

---
+++ Viewing/Models/Auth/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Viewing.Models.Auth
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        public string? ErrorMessage { get; set; }
+    }
+}
