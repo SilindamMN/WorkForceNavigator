db1fd9883538ce9c7834d34141b690deb825540c
SilindamMN <silindanemdu@gmail.com>
2026-07-09 00:49:12 +0200

    fix values returned (#41)

--- Domain/Dtos/Account/UserDetailsDto.cs
+++ Domain/Dtos/Account/UserDetailsDto.cs
@@ -13,11 +13,15 @@
     public string Username { get; set; }
     public IEnumerable<string> Roles { get; set; }
     public Gender? Gender { get; set; }
-    public string? JobTitle { get; set; }
+        public int? JobTitleId { get; set; }
+        public string? JobTitle { get; set; }
     public decimal? Salary { get; set; }
-    public string? LineManager { get; set; }
-    public string Department { get; set; }
-    public Seniority Seniority { get; set; }
+
+        public int? DepartmentId { get; set; }
+        public string Department { get; set; }
+        public int? TeamId { get; set; }
+        public string TeamName { get; set; }
+        public Seniority Seniority { get; set; }
     public DateTime JoiningDate { get; set; }
     public string PhoneNumber { get; set; }
   }
