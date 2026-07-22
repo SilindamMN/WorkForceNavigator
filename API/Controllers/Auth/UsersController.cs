c4e21bd227d215df7089732a851bf4ee4f87cc5e
Mduduzi <silindanemdu@gmail.com>
2026-07-22 13:13:34 +0200

    add changes

---
+++ API/Controllers/Auth/UsersController.cs
@@ -0,0 +1,73 @@
+﻿namespace API.Controllers.Auth
+{
+    using Application.Dtos.Account.Users;
+    using Application.Dtos.Hr.JobTitles;
+    using Application.Interfaces.Auth;
+    using Application.Services.Auth;
+    using Domain.Constants;
+    using Domain.Dtos.General;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Persistence;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+        private readonly IUserJobTitleService userJobTitleService;
+
+        public UsersController(IUserService userService, IUserJobTitleService userJobTitleService)
+        {
+            this.userService = userService;
+            this.userJobTitleService = userJobTitleService;
+        }
+
+        [HttpGet]
+        [Route("jobtitle/{username}")]
+        public async Task<ActionResult<JobTitleDto>> GetJobTitleByUsername([FromRoute] string username)
+        {
+            var user = await userJobTitleService.GetJobTitleForUser(username);
+            return user is null ? NotFound("Username Not Found") : Ok(user);    
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserInfoResult>>> GetUsersList()
+        {
+            var userList = await userService.GetUserListAsync();
+            return Ok(userList);
+        }
+
+        [HttpGet]
+        [Route("users/{username}")]
+        public async Task<ActionResult<UserDetailsDto>> GetUserDetailsByUsernames([FromRoute] string username)
+        {
+            var user = await userService.GetUserDetailsByUserNameAsync(username);
+            return user is null ? NotFound("Username Not Found") : Ok(user);
+        }
+
+        [HttpGet]
+        [Route("userDetails/{username}")]
+        public async Task<ActionResult<UserDetailsDto>> GetUserExtraDetailsByUsername([FromRoute] string username)
+        {
+            var user = await userService.GetUserExtraDetailsByUserNameAsync(username);
+            return user is null ? NotFound("Username Not Found") : Ok(user);
+        }
+        [HttpGet]
+        [Route("usernames")]
+        public async Task<ActionResult<IEnumerable<string>>> GetUsernameList()
+        {
+            var usernames = await userService.GetUsernamesListAsync();
+            return Ok(usernames);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public async Task<IActionResult> UpdateUserDetails(string updateUsername, int departmentId, [FromBody] UpdateUserDetailsDto userDetailsDto)
+        {
+            var updateResult = await userService.UpdateUserDetailsAsync(updateUsername, departmentId, userDetailsDto);
+            return StatusCode(updateResult.StatusCode,updateResult.Message);
+        }
+    }
+}
---
+++ Application/Services/Auth/UserService.cs
@@ -0,0 +1,170 @@
+﻿using Application.Dtos.Account.Users;
+using Application.Helpers;
+using Application.Interfaces.Auth;
+using Domain.Account;
+using Domain.Constants.Enums;
+using Domain.Dtos.General;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Auth
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly DataContext dataContext;
+        private readonly IUserJobTitleService userJobTitleService;
+
+        public UserService(UserManager<ApplicationUser> userManager, DataContext dataContext, IUserJobTitleService userJobTitleService)
+        {
+            this.userManager = userManager;
+            this.dataContext = dataContext;
+            this.userJobTitleService = userJobTitleService;
+        }
+
+        public  Task<UserDetailsDto> GetUserExtraDetailsByUserNameAsync(string userName)
+        {
+            var user =  userManager.FindByNameAsync(userName).Result;
+            if (user is null) { return null; }
+
+            var roles =  userManager.GetRolesAsync(user).Result;
+            var userInfor =  GenerateUserInfo(user, roles, userJobTitleService);
+            return userInfor;
+        }
+
+        public async Task<IEnumerable<UserInfoResult>> GetUserListAsync()
+        {
+            var users = await userManager.Users
+        .Include(u => u.JobTitle)
+            .ThenInclude(jt => jt.Department)
+        .Include(u => u.UserTeams)
+            .ThenInclude(ut => ut.Team)
+                .ThenInclude(t => t.Department)
+        .ToListAsync();
+
+            List<UserInfoResult> userInfoResults = new List<UserInfoResult>();
+
+            foreach (var user in users)
+            {
+                var roles = await userManager.GetRolesAsync(user);
+                var userInfo = await GenerateUserInfoAsync(user, roles);
+                userInfoResults.Add(userInfo);
+            }
+            return userInfoResults;
+        }
+
+        public async Task<IEnumerable<string>> GetUsernamesListAsync()
+        {
+            return await userManager.Users.Select(u => u.UserName).ToListAsync();
+        }
+
+        public async Task<UserInfoResult> GenerateUserInfoAsync(ApplicationUser user, IList<string> roles)
+        {
+            var currentUserTeam = await dataContext.UserTeams
+           .Where(ut => ut.UserId == user.Id)
+           .Select(ut => new
+           {
+               ut.TeamId,
+               TeamName = ut.Team.TeamName ?? string.Empty,
+               DepartmentId = ut.Team.DepartmentId,
+               DepartmentName = ut.Team.Department.DepartmentName
+           })
+           .FirstOrDefaultAsync();
+
+            var userTitle = await userJobTitleService.GetJobTitleForUser(user.UserName);
+
+            return new UserInfoResult
+            {
+                Id = user.Id,
+                CreatedAt = DateTime.Now,
+                Email = user.Email ?? string.Empty,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                TeamId = currentUserTeam?.TeamId,
+                TeamName = currentUserTeam?.TeamName ?? string.Empty,
+                DepartmentId = currentUserTeam?.DepartmentId,
+                DepartmentName = currentUserTeam?.DepartmentName ?? string.Empty,
+                JobTitleId = user.JobTitleId,
+                JobTitleName = userTitle?.Title ?? string.Empty,
+                Roles = roles,
+                Seniority = user.Seniority,
+                Username = user.UserName,
+                PhoneNumber = user.PhoneNumber ?? string.Empty,
+                Gender = (Gender?)user.Gender
+            };
+        }
+        public Task<UserInfoResult> GetUserDetailsByUserNameAsync(string userName)
+        {
+            var user = userManager.FindByNameAsync(userName).Result;
+            if (user is null) { return null; }
+
+            var roles = userManager.GetRolesAsync(user).Result;
+            var userInfor = GenerateUserInfoAsync(user, roles);
+            return userInfor;
+        }
+
+        public async Task<GeneralServiceResponseDto> UpdateUserDetailsAsync(string username, int departmentId, UpdateUserDetailsDto userDetailsDto)
+        {
+            var user = await dataContext.Users
+                .Include(u => u.JobTitle)
+                .Where(x => x.UserName == username)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                return ResponseHelper.CreateResponse(false, StatusCodes.Status404NotFound, "User Not Found");
+            }
+            var jobTitles = await userJobTitleService.GetJobTitleByDepartmentAndSeniorityAsync(departmentId, user.Seniority);
+
+            var validJobTitle = jobTitles.Any(j => j.JobTitleId == userDetailsDto.JobTitleId);
+
+            if (!validJobTitle)
+            {
+                return ResponseHelper.CreateResponse(false, StatusCodes.Status412PreconditionFailed, "Invalid Job Title for this department");
+            }
+
+            user.FirstName = userDetailsDto.FirstName;
+            user.LastName = userDetailsDto.LastName;
+            user.Gender = userDetailsDto.Gender;
+            user.Salary = userDetailsDto.Salary;
+            user.PhoneNumber = userDetailsDto.Phonenumber;
+            user.JobTitleId = userDetailsDto.JobTitleId;
+            user.Seniority = Enum.Parse<Seniority>(
+    jobTitles.First(x => x.JobTitleId == userDetailsDto.JobTitleId).Seniority);
+            await dataContext.SaveChangesAsync();
+
+            return new GeneralServiceResponseDto { };
+        }
+        private async Task<UserDetailsDto> GenerateUserInfo(ApplicationUser user, IList<string> roles, IUserJobTitleService userJobTitleService)
+        {
+            var details = await userJobTitleService.GetJobTitleForUser(user.UserName);
+            var userInfo = new UserDetailsDto
+            {
+                Id = user.Id,
+                JoiningDate = user.CreatedAt,
+                Email = user.Email ?? string.Empty,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Roles = roles,
+                Username = user.UserName,
+                Salary = user.Salary,
+                DepartmentId = user.JobTitle?.DepartmentId,
+                Department = details.DepartmentName ?? string.Empty,
+                JobTitleId = user.JobTitleId,
+                JobTitle = details.Title,
+                TeamId = user.UserTeams.FirstOrDefault()?.TeamId,
+                TeamName = user.UserTeams?.FirstOrDefault()?.Team?.TeamName ?? string.Empty,
+                PhoneNumber = user?.PhoneNumber ?? string.Empty,
+                Gender = (Gender)user.Gender,
+            };
+            return userInfo;
+        }
+    }
+}
