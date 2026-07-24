98e9a0f6d0028a2bd391f9fd1a455ca5830d30cb
Mduduzi Silinda <silindanemdu@gmail.com>
2026-07-24 21:28:21 +0200

    push

--- API/Controllers/Hr/DepartmentController.cs
+++ API/Controllers/Hr/DepartmentController.cs
@@ -14,103 +14,103 @@ namespace API.Controllers.Hr
     using Microsoft.AspNetCore.Mvc;
 
     [ApiController]
-  [Route("api/departments")]
-  public class DepartmentController : ControllerBase
-  {
-    private readonly IGenericService<Department, DepartmentDto> _DepartmentService;
-    private readonly IGenericService<Department, UpdateDepartmentDto> updateDepartmentService;
-    private readonly IDepartmentService departmentService;
-
-    public DepartmentController(IDepartmentService departmentService,
-        IGenericService<Department, DepartmentDto> DepartmentService, IGenericService<Department, UpdateDepartmentDto> updateDepartmentService)
-
+    [Route("api/departments")]
+    public class DepartmentController : ControllerBase
     {
-      _DepartmentService = DepartmentService;
-      this.updateDepartmentService = updateDepartmentService;
-      this.departmentService = departmentService;
-    }
+        private readonly IGenericService<Department, DepartmentDto> _DepartmentService;
+        private readonly IGenericService<Department, UpdateDepartmentDto> updateDepartmentService;
+        private readonly IDepartmentService departmentService;
 
-    [HttpGet]
-    public async Task<IActionResult> GetAllDepartments()
-    {
-      var result = await _DepartmentService.GetAllAsync();
+        public DepartmentController(IDepartmentService departmentService,
+            IGenericService<Department, DepartmentDto> DepartmentService, IGenericService<Department, UpdateDepartmentDto> updateDepartmentService)
 
-      return Ok(result);
-    }
+        {
+            _DepartmentService = DepartmentService;
+            this.updateDepartmentService = updateDepartmentService;
+            this.departmentService = departmentService;
+        }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetDepartmentById(int id)
-    {
-      var result = await _DepartmentService.GetByIdAsync(id);
-      if (result is null)
-      {
-        return NotFound("leaveRequestId not found");
-      }
-      else
-      {
-        return Ok(result);
-      }
-    }
+        [HttpGet]
+        public async Task<IActionResult> GetAllDepartments()
+        {
+            var result = await _DepartmentService.GetAllAsync();
 
-    [HttpGet]
-    [Route("jobtitle/{id}")]
-    public async Task<ActionResult<List<UserDetailJobTitle>>> GetDepartmentUserDetailJobTitle(int id)
-    {
-      var result = await departmentService.GetUserJobTitleTeamsListAsync(id);
-      if (result == null || !result.Any())
-      {
-        return NotFound($"No details found for departmentId: {id}");
-      }
+            return Ok(result);
+        }
 
-      return Ok(result);
-    }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDepartmentById(int id)
+        {
+            var result = await _DepartmentService.GetByIdAsync(id);
+            if (result is null)
+            {
+                return NotFound("leaveRequestId not found");
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
 
-    [HttpPost("CreateDepartment")]
-    public async Task<IActionResult> CreateDepartment([FromBody] DepartmentDto departmentDto)
-    {
-      var result = await _DepartmentService.CreateAsync(departmentDto);
-      if (result.IsSucceed)
-      {
-        return Ok(result.Message);
-      }
-      return StatusCode(result.StatusCode, result.Message);
-    }
+        [HttpGet]
+        [Route("jobtitle/{id}")]
+        public async Task<ActionResult<List<UserDetailJobTitle>>> GetDepartmentUserDetailJobTitle(int id)
+        {
+            var result = await departmentService.GetUserJobTitleTeamsListAsync(id);
+            if (result == null || !result.Any())
+            {
+                return NotFound($"No details found for departmentId: {id}");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("department")]
+        public async Task<IActionResult> CreateDepartment([FromBody] DepartmentDto departmentDto)
+        {
+            var result = await _DepartmentService.CreateAsync(departmentDto);
+            if (result.IsSucceed)
+            {
+                return Ok(result.Message);
+            }
+            return StatusCode(result.StatusCode, result.Message);
+        }
 
         [HttpPatch("{id}")]
         public async Task<IActionResult> UpdateDepartment(int id, [FromBody] UpdateDepartmentDto updateDepartmentDto)
-    {
-      var result = await updateDepartmentService.UpdateAsync(id, updateDepartmentDto);
-      if (result.IsSucceed)
-      {
-        return Ok(result.Message);
-      }
-      else
-      {
-        return StatusCode(result.StatusCode, result.Message);
-      }
-    }
+        {
+            var result = await updateDepartmentService.UpdateAsync(id, updateDepartmentDto);
+            if (result.IsSucceed)
+            {
+                return Ok(result.Message);
+            }
+            else
+            {
+                return StatusCode(result.StatusCode, result.Message);
+            }
+        }
 
-    [HttpDelete]
-    [Route("delete")]
-    public async Task<IActionResult> SoftDeleteDepartment(int id)
-    {
-      var result = await _DepartmentService.SoftDeleteAsync(id);
-      if (result.IsSucceed)
-      {
-        return Ok(result.Message);
-      }
-      return StatusCode(result.StatusCode, result.Message);
-    }
+        [HttpDelete]
+        [Route("delete")]
+        public async Task<IActionResult> SoftDeleteDepartment(int id)
+        {
+            var result = await _DepartmentService.SoftDeleteAsync(id);
+            if (result.IsSucceed)
+            {
+                return Ok(result.Message);
+            }
+            return StatusCode(result.StatusCode, result.Message);
+        }
 
-    [HttpDelete("{id}/undo")]
-    public async Task<IActionResult> UnSoftDeleteDepartment(int id)
-    {
-      var result = await _DepartmentService.UndoSoftDeleteAsync(id);
-      if (result.IsSucceed)
-      {
-        return Ok(result.Message);
-      }
-      return StatusCode(result.StatusCode, result.Message);
+        [HttpDelete("{id}/undo")]
+        public async Task<IActionResult> UnSoftDeleteDepartment(int id)
+        {
+            var result = await _DepartmentService.UndoSoftDeleteAsync(id);
+            if (result.IsSucceed)
+            {
+                return Ok(result.Message);
+            }
+            return StatusCode(result.StatusCode, result.Message);
+        }
     }
-  }
 }
