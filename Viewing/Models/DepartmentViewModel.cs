cca8ba021498a06e0ff1b4834230530eb1b7b366
SilindamMN <silindanemdu@gmail.com>
2026-08-19 14:08:28 +0200

    removed old frontendui

--- Viewing/Models/DepartmentViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Viewing.Models
-{
-    public class DepartmentViewModel
-    {
-        public int Id { get; set; }
-        public string DepartmentName { get; set; }
-        public string Description { get; set; }
-    }
-}
