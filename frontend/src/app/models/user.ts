78948af3197daf66d80c8425e9977fb78f4ad87c
SilindamMN <silindanemdu@gmail.com>
2026-09-08 16:56:43 +0200

    fixed (#133)

--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -1,41 +1,73 @@
 import { Gender } from "./Constant/enums/gender";
+import { Seniority } from "./Constant/enums/seniority";
 
 export interface User {
+
   id?: any;
+
   firstName: string;
+
   lastName: string;
+
   email: string;
+
   username: string;
+
   phoneNumber?: string | null;
+
   salary?: string | null;
+
   gender?: Gender | null;
+
   roles: string[];
 }
 
-// DTO without id and createdAt
 export interface UserDto {
+
   id?: any;
+
   departmentId?: number | null;
+
   jobTitleId?: number | null;
+
   teamId?: number | null;
+
   firstName: string;
+
   lastName: string;
+
   email: string;
+
   username: string;
+
   phoneNumber?: string | null;
+
   salary?: string | null;
+
   jobTitle?: string | null;
+
   gender?: Gender | null;
+
+  seniority?: Seniority | null;
+
   roles: string[];
 }
 
 export interface UpdateUserDetailsDto {
+
   firstName: string;
+
   lastName: string;
+
   gender: string;
+
   jobTitleId: number;
+
   teamId?: number | null;
+
   seniority?: string | null;
+
   salary?: number | null;
+
   phonenumber: string;
 }
--- frontend/src/app/shared/services/jobtiteservice.service.ts
+++ frontend/src/app/shared/services/jobtiteservice.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { GenericCrudService } from './generic.service';
 import { HttpClient } from '@angular/common/http';
 import { JobTitle } from '../../models/jobtitle';
+import { Observable } from 'rxjs';
+import { Seniority } from '../../models/Constant/enums/seniority';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +11,10 @@ import { JobTitle } from '../../models/jobtitle';
 export class JobtiteserviceService extends GenericCrudService<JobTitle> {
   constructor(http: HttpClient) {
     super(http, 'jobtitles');
+  }  getJobTitleByDepartmentId(departmentId: number, seniority: Seniority): Observable<JobTitle[]> {
+    return this.http.get<JobTitle[]>(
+      `${this.baseUrl}/Department/${departmentId}?seniority=${seniority}`
+    );
   }
   
 }
